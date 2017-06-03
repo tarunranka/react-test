f7e36640a1b7e6f6ba5f3fc4ac34a15ae0bbc6ad
Tarun Ranka <tarun.ranka@sharafdg.com>
2017-06-03 11:26:13 +0400

    updated son

--- client/reducers/reducer_products.js
+++ client/reducers/reducer_products.js
@@ -4,13 +4,13 @@ export default function() {
       "seller":[
         {
           "sid":1,
-          "image":"/image/profile-1.jpg",
+          "image":"./image/profile-1.jpg",
           "name":"Tarun Ranka",
           "distance":"500m"
         }
       ],
-      "image":"/image/ProductRed-iPhone-7-1.jpg",
-      "gallery":["/image/ProductRed-iPhone-7.jpg","/image/ProductRed-iPhone-7-1.jpg","/image/ProductRed-iPhone-7-2.png"],
+      "image":"./image/ProductRed-iPhone-7-1.jpg",
+      "gallery":["./image/ProductRed-iPhone-7.jpg","./image/ProductRed-iPhone-7-1.jpg","./image/ProductRed-iPhone-7-2.png"],
       "pid":1,
       "name":"iPhone 7 Red",
       "desc":"iPhone 7 is also available in Jet Black, Black, Silver, Gold, and Rose Gold.",
@@ -21,13 +21,13 @@ export default function() {
         {
 
           "name":"Tarun",
-          "avatar":"/image/profile-1.jpg",
+          "avatar":"./image/profile-1.jpg",
           "comment":"Lorem ipsum dolor sit amet, consectetur adipicing eli"
         },
         {
 
           "name":"Siva",
-          "avatar":"/image/profile-2.jpg",
+          "avatar":"./image/profile-2.jpg",
           "comment":"Lorem ipsum dolor sit amet, consectetur adipicing eli"
         }
       ]
@@ -36,15 +36,15 @@ export default function() {
       "seller":[
         {
           "sid":1,
-          "image":"/image/profile-2.jpg",
+          "image":"./image/profile-2.jpg",
           "name":"Siva",
           "distance":"500m"
         }
       ],
       "pid":2,
       "name":"Samsung Galaxy 8",
-      "image":"/image/samsung-s8.jpg",
-      "gallery":["/image/samsung-s8.jpg","/image/samsung-s8-1.jpg"],
+      "image":"./image/samsung-s8.jpg",
+      "gallery":["./image/samsung-s8.jpg","./image/samsung-s8-1.jpg"],
       "desc":"Samsung Galaxy 8 is available in Midnight Black, Orchid Gray, Coral Blue, Arctic Silver, Maple Gold",
       "hashTags":["#samsung","#s8","#samsung galaxy s8"],
       "price": "AED 3,799",
@@ -52,7 +52,7 @@ export default function() {
       "comments":[
         {
           "name":"Tarun",
-          "avatar":"/image/profile-1.jpg",
+          "avatar":"./image/profile-1.jpg",
           "comment":"Lorem ipsum dolor sit amet, consectetur adipicing eli"
         }
       ]
@@ -61,13 +61,13 @@ export default function() {
       "seller":[
         {
           "sid":1,
-          "image":"/image/profile-3.jpg",
+          "image":"./image/profile-3.jpg",
           "name":"Guna",
           "distance":"300m"
         }
       ],
-      "image":"/image/iphone7-gallery1.jpeg",
-      "gallery":["/image/iphone7-gallery1.jpeg","/image/iphone7-gallery2.jpg"],
+      "image":"./image/iphone7-gallery1.jpeg",
+      "gallery":["./image/iphone7-gallery1.jpeg","./image/iphone7-gallery2.jpg"],
       "pid":3,
       "name":"iPhone 7",
       "desc":"iPhone 7 is also available in Jet Black, Black, Silver, Gold, and Rose Gold.",
@@ -78,13 +78,13 @@ export default function() {
         {
 
           "name":"Tarun",
-          "avatar":"/image/profile-1.jpg",
+          "avatar":"./image/profile-1.jpg",
           "comment":"Lorem ipsum dolor sit amet, consectetur adipicing eli"
         },
         {
 
           "name":"Siva",
-          "avatar":"/image/profile-2.jpg",
+          "avatar":"./image/profile-2.jpg",
           "comment":"Lorem ipsum dolor sit amet, consectetur adipicing eli"
         }
       ]
@@ -93,13 +93,13 @@ export default function() {
       "seller":[
         {
           "sid":1,
-          "image":"/image/profile-3.jpg",
+          "image":"./image/profile-3.jpg",
           "name":"Guna",
           "distance":"300m"
         }
       ],
-      "image":"/image/iphone6-gallery1.jpg",
-      "gallery":["/image/iphone6-gallery1.jpg","/image/iphone6-gallery2.jpg"],
+      "image":"./image/iphone6-gallery1.jpg",
+      "gallery":["./image/iphone6-gallery1.jpg","./image/iphone6-gallery2.jpg"],
       "pid":5,
       "name":"iPhone 6",
       "desc":"iPhone 6 is also available in Jet Black, Black, Silver, Gold, and Rose Gold.",
@@ -110,13 +110,13 @@ export default function() {
         {
 
           "name":"Tarun",
-          "avatar":"/image/profile-1.jpg",
+          "avatar":"./image/profile-1.jpg",
           "comment":"Lorem ipsum dolor sit amet, consectetur adipicing eli"
         },
         {
 
           "name":"Siva",
-          "avatar":"/image/profile-2.jpg",
+          "avatar":"./image/profile-2.jpg",
           "comment":"Lorem ipsum dolor sit amet, consectetur adipicing eli"
         }
       ]
